14e5aeeb3c63a8afc49fb70e93719435bfd2286f
Keith Lo <keith-lo@outlook.com>
2017-01-08 21:56:28 +0800

    added report

--- src/app/pages/page-invoice/page-invoice.component.ts
+++ src/app/pages/page-invoice/page-invoice.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
 
+import { Customer, CustomersService } from '../../services/customers/customers.service';
 import { TransactionsService, AccountReceivalbe, PaymentTransaction } from '../../services/transactions/transactions.service';
 import { HttpService } from '../../services/http/http.service';
 
@@ -9,22 +10,35 @@ import { HttpService } from '../../services/http/http.service';
   selector: 'app-page-invoice',
   templateUrl: './page-invoice.component.html',
   styleUrls: ['./page-invoice.component.sass'],
-  providers: [TransactionsService, HttpService]
+  providers: [TransactionsService, CustomersService, HttpService]
 })
 export class PageInvoiceComponent implements OnInit {
 
   private _params : any;
 
+  public customer: Customer = new Customer();
+
   constructor(
     private _activatedRoute: ActivatedRoute,
-    private _transactions: TransactionsService
+    private _transactions: TransactionsService,
+    private _customer: CustomersService
   ) { }
 
   ngOnInit() {
-    console.log('invoice loaded.');
     this._params = this._activatedRoute.params.subscribe(params => {
-      console.log('received params', params);
-      let monthStr: string = ( params['id'] || '' );
+      let customerId: number = parseInt(( params['id'] || '' ));
+      
+      this._customer.get(customerId).subscribe(
+        customer => {
+          this.customer = customer
+          this._customer.joinedPurposes(customer).subscribe(
+            purposes => {
+              this.customer = customer;
+              console.log(this.customer);
+            }
+          );
+        }
+      );
     });
   }
 
